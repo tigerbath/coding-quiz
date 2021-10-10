550f76165ed5357334c2d4866daa3eb9c4911ca4
Matthew Palmer <mattjpalmer92@gmail.com>
2021-10-10 17:50:35 +0100

    made ammendments to header, added new typeface from gfonts

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,27 +1,29 @@
 //questions array
 var allQuestions = {[
-    question: "Commonly used data types DO NOT include",
+    question: 'Commonly used data types DO NOT include',
     choices: ['strings', 'booleans', 'alerts', 'numbers'],
     correctAnswer: 2
 },
 {
-    question: "The condition in an if / else statement is enclosed within ____ ",
+    question: 'The condition in an if / else statement is enclosed within ____ ',
     choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
     correctAnswer: 2
 },
 {
-    question: "arrays in JavaScript can be used to store ______"
+    question: 'arrays in JavaScript can be used to store ______'
     choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above']
     correctAnswer: 3
 },
 {
-    question: "String values must be enclosed within ______  when being assigned to variables"
+    question: 'String values must be enclosed within ______  when being assigned to variables'
     choices: ['commas', 'curly brackets', 'quotes', 'parentheses']
     correctAnswer: 2
 },
 {
-    question: "A very useful tool during development and debugging for printing content to the debugger is"
+    question: 'A very useful tool during development and debugging for printing content to the debugger is'
     choices: ['JavaScript', 'Terminal/bash', 'for loops', 'console.log']
     correctAnswer: 3
 }
-}];
+];
+
+
